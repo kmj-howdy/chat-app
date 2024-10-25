0abf1fdca6006d1d6fa888a3d2d0835115b640ee
kmj-howdy <wen0414@naver.com>
2024-10-25 22:47:45 +0900

    docs: 인터페이스 설명 추가

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -1,10 +1,12 @@
 export type ChatModelId = 'test-model-001' | 'test-model-002' | 'test-model-003';
 export type ChatModelName = 'Nota_Model_01' | 'Nota_Model_02' | 'Nota_Model_03';
 
-// TODO: 대화 주체 추가 필요
 export type Dialogue = {
+  /** 채팅 쌍의 id */
   dialogue_id: string;
+  /** 질문 */
   prompt: string;
+  /** 답변 */
   completion: string;
 };
 

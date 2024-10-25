5b6ff814f90cd8bb62c14d73b1733eb0ac31f54f
kmj-howdy <wen0414@naver.com>
2024-10-25 17:24:14 +0900

    feat: App > GlobalStyle, ThemeProvider 적용

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,16 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
+import styled, { ThemeProvider } from 'styled-components';
+import GlobalStyle from '@/styles/GlobalStyle';
+import theme from '@/styles/theme';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #bf4f74;
+  background-color: ${({ theme }) => theme.colors.main};
+`;
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,22 +26,26 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Title>제목 테스트</Title>
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      </ThemeProvider>
     </>
   );
 }

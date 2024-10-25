253b54f55e661dbf8df090deb8eedf98ac3de0e4
kmj-howdy <wen0414@naver.com>
2024-10-25 18:04:01 +0900

    feat: App > Main 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { useEffect } from 'react';
 
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '@/styles/GlobalStyle';
 import theme from '@/styles/theme';
-
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: #bf4f74;
-  background-color: ${({ theme }) => theme.colors.main};
-`;
+import Main from './pages/Main';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   useEffect(() => {
     // 예시를 위한 코드 입니다. (지우셔도 무방합니다.)
     fetch('/test')
@@ -28,23 +17,7 @@ function App() {
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
-        <Title>제목 테스트</Title>
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+        <Main />
       </ThemeProvider>
     </>
   );

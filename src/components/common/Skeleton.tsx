c7fed7c07f3e4d7bbe6c3c66f1018722cf6cfed8
kmj-howdy <wen0414@naver.com>
2024-10-28 00:44:21 +0900

    feat: width props 추가

--- src/components/common/Skeleton.tsx
+++ src/components/common/Skeleton.tsx
@@ -6,9 +6,13 @@ const shimmer = keyframes`
   100% { background-position: 200px 0; }
 `;
 
-const SkeletonBox = styled.div<{ $height: CSSProperties['height'] }>`
+const SkeletonBox = styled.div<{
+  $height: CSSProperties['height'];
+  $width: CSSProperties['width'];
+}>`
   width: 100%;
   height: ${({ $height }) => $height || '2rem'};
+  width: ${({ $width }) => $width || '5rem'};
   background: #e0e0e0;
   background-image: linear-gradient(90deg, #e0e0e0 25%, #f0f0f0 50%, #e0e0e0 75%);
   background-size: 200% 100%;
@@ -18,8 +22,11 @@ const SkeletonBox = styled.div<{ $height: CSSProperties['height'] }>`
 
 type SkeletonProps = {
   height?: CSSProperties['height'];
+  width?: CSSProperties['width'];
 };
 
-const Skeleton = ({ height }: SkeletonProps) => <SkeletonBox $height={height} />;
+const Skeleton = ({ height, width }: SkeletonProps) => {
+  return <SkeletonBox $height={height} $width={width} />;
+};
 
 export default Skeleton;

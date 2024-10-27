025d8de06a1b80d0bd80c84b4b375c9a86536dfb
kmj-howdy <wen0414@naver.com>
2024-10-27 20:11:51 +0900

    chore: 미사용 코드 제거

--- src/apis/chats.ts
+++ src/apis/chats.ts
@@ -1,25 +1,5 @@
-import { Chat, ChatModels } from '@/types/chat';
+import { Chat } from '@/types/chat';
 import request from '@/utils/request';
-
-interface FetchChatsAndModelsFunc {
-  (): Promise<{ chatsData: Chat[]; chatModels: ChatModels[] } | undefined>;
-}
-
-export const fetchChatsAndModels: FetchChatsAndModelsFunc = async () => {
-  try {
-    const [fetchedChats, fetchedChatModels] = await Promise.all([
-      request.get<Chat[]>('/chats'),
-      request.get<ChatModels[]>('/chat_model'),
-    ]);
-
-    return {
-      chatsData: fetchedChats,
-      chatModels: fetchedChatModels,
-    };
-  } catch (error) {
-    console.error(error);
-  }
-};
 
 export const fetchChats = async () => {
   const result = await request.get<Chat[]>('/chats');

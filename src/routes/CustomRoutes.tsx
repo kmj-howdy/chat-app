03b38a2236b00b46361757558fc1bae2e964d8a2
kmj-howdy <wen0414@naver.com>
2024-10-27 20:23:17 +0900

    feat: 새로운 채팅화면, 기존 채팅화면 컴포넌트 생성

--- src/routes/CustomRoutes.tsx
+++ src/routes/CustomRoutes.tsx
@@ -1,11 +1,22 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from '../pages/Main';
+import { lazy } from 'react';
+
+const NewChatScreen = lazy(() => import('@/components/NewChatScreen'));
+const ExistChatScreen = lazy(() => import('@/components/ExistChatScreen'));
 
 const router = createBrowserRouter([
   {
     path: '/chats',
     element: <Main />,
     errorElement: <div>에러 404</div>,
+    children: [
+      { path: '/chats', element: <NewChatScreen /> },
+      {
+        path: ':chatId',
+        element: <ExistChatScreen />,
+      },
+    ],
   },
 ]);
 
